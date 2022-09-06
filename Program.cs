e44117ea54a49cb29b8463c769f3cc78c859297e
ANIL AYAYDIN <anil.ayaydin@gmail.com>
2022-09-06 16:09:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Algorithms {
+    class Program {
+
+        static void Main(string[] args) {
+            LinkedList<string> listy = new LinkedList<string>();
+            //AddLast
+            //AddFirst
+            listy.AddLast("Sinan");
+            listy.AddLast("Anıl");
+            listy.AddLast("Mustafa");
+            listy.AddLast("Tayfun");
+
+            Console.WriteLine(listy.Contains("Anıl"));
+            Console.WriteLine(listy.Contains("Mustafa"));
+            Console.WriteLine(listy.Contains("Tayfun"));
+            Console.WriteLine(listy.Contains("Pand"));
+
+            Console.WriteLine(listy.Count);
+            listy.RemoveFirst();
+
+            foreach (string item in listy)
+            {
+                Console.Write(item + "->");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
